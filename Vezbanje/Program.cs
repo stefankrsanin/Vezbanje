e34bb18cf21f46af60bfeec400920b48d237529c
stefan.krsanin <stefan.krsanin@KRSANINSTE>
2023-04-11 09:04:44 +0200

    create class with constructor

--- Vezbanje/Program.cs
+++ Vezbanje/Program.cs
@@ -8,8 +8,32 @@ namespace Vezbanje
 {
     internal class Program
     {
+        public class Employee
+        {
+            public string name;
+            public string lastName;
+            public int age;
+
+
+
+            public Employee(string _name, string _lastName, int _age)
+            {
+                name = _name;
+                lastName = _lastName;
+                age = _age;
+
+                Console.WriteLine("This is emplyee with this parametar: ");
+            }
+
+        }
+
         static void Main(string[] args)
         {
+            
+            Employee emp = new Employee("Stefan", "Krsanin", 28);
+            Console.WriteLine(emp.name);
+            Console.WriteLine(emp.lastName);
+            Console.WriteLine(emp.age);
         }
     }
 }
